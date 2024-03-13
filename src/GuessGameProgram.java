c48dcf73882be6b63e24b0d2775379e91faccc01
cristic1988 <cristic8@gmail.com>
2024-03-13 23:24:08 +0200

    second commit

--- src/GuessGameProgram.java
+++ src/GuessGameProgram.java
@@ -1,17 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessGameProgram {
 
     public static void main (String[] args) {
 
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+
+
         int guessNum = random.nextInt(10);
+          
+        while ( guessNum < 3) {
+              System.out.println("Enter number to guess");
+            int compNum = scanner.nextInt();
+            System.out.println(compNum);
 
-
-        int i = random.nextInt(10);
-        while (i < 3) {
             System.out.println(guessNum);
-            i++;
+            guessNum++;
 
         }
 
